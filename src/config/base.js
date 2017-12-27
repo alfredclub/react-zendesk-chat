92edc84a1431f10b3e1f86b4b6b6443af9058d62
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-12-27 11:47:29 -0400

    Removes config folder

--- src/config/base.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Settings configured here will be merged into the final config object.
-export default {
-	ACCOUNT_KEY: '',
-	// Set to 'docked' or 'normal' for docked or normal mode respectively
-	THEME: 'normal'
-}
